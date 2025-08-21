fd62eabdc62f5f111cb383b4707499a9e5111095
Madhavi Tripathi <madhavi@chandrakala.co.in>
2025-08-21 12:56:35 +0530

    Project Config model changed

--- Models/ProjectConfig.cs
+++ Models/ProjectConfig.cs
@@ -10,9 +10,9 @@ namespace Tools.Models
         public int Id { get; set; }
         public List<int> Modules { get; set; }
         public int ProjectId { get; set; }
-        public List<int> MergeKey { get; set; }
-        public List<int> PackagingSort { get; set; }
-        public List<int> EnvelopeSort { get; set; }
+        public string Envelope { get; set; }
+        public List<int> BoxBreaking { get; set; }
+        public string Extras { get; set; }
 
     }
 }
